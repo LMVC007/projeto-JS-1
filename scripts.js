9628b7507072489f79dff6721b67fbbccedffa50
LMVC007 <luizmiguelvieiracarvalho0@gmail.com>
2026-05-24 21:55:09 -0300

    Adicionando o "Euro" no JS.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,39 @@
 const convertButton = document.querySelector('.convert-button')
+const currencySelect = document.querySelector('.currency-select')
 
 function convertCurrency() {
     const inputCurrencyValue = document.querySelector('.input-currency').value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValue = document.querySelector(".currency-value")
 
+    console.log(currencySelect.value)
+
     const dolarToday = 5.2
+    const euroToday = 5.9
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    if (currencySelect.value == "dolar") {
+        currencyValue.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
 
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
+    }
+    if (currencySelect.value == "euro") {
+        currencyValue.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
 
-    currencyValue.innerHTML = new Intl.NumberFormat("en-US",{
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)  
 
-   
+
+
 }
 
 convertButton.addEventListener("click", convertCurrency)
